d174a3385f42cd171038c566c499ae4e7601d811
Hadi Rezaei <hadirezaei.etc.esfahan@gmail.com>
2022-05-31 16:48:30 +0430

    comments

--- Defer.go
+++ Defer.go
@@ -1,5 +1,9 @@
 package main
 
+//Defer is used to ensure that a function call is performed later in a program’s
+// execution, usually for purposes of cleanup. defer is often used where e.g.
+//ensure and finally would be used in other languages.
+
 import (
 	"fmt"
 	"os"
@@ -7,10 +11,17 @@ import (
 
 func main() {
 
+	//Suppose we wanted to create a file, write to it, and then close when we’re done.
+	//Here’s how we could do that with defer.
+
 	f := createFile("/tmp/defer.txt")
 	defer closeFile(f)
 	writeFile(f)
 }
+
+//Immediately after getting a file object with createFile, we defer the closing
+//of that file with closeFile. This will be executed at the end of the enclosing
+//function (main), after writeFile has finished.
 
 func createFile(p string) *os.File {
 	fmt.Println("creating")
@@ -36,3 +47,13 @@ func closeFile(f *os.File) {
 		os.Exit(1)
 	}
 }
+
+//It’s important to check for errors when closing a file, even in a deferred function.
+
+//Running the program confirms that the file is closed after being written.
+
+//output:
+
+//creating
+//writing
+//closing
